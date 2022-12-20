e341f41b2eb8ae808291ea9bbbc830ac3fd0e034
Charles Arthur <charles.v.arthur@gmail.com>
2022-12-20 11:30:13 +0000

    updated link

--- penguins.py
+++ penguins.py
@@ -7,5 +7,5 @@ import pandas as pd
 st.title("Palmers Penguins")
 
 #Data imports
-penguins_df = pd.read_csv("https://github.com/charlesvarthur/Streamlit-for-Data-Science/blob/24f999a9c033b0a84480999f6553cf59c783f4e8/penguin_app/penguins.csv")
+penguins_df = pd.read_csv("https://github.com/charlesvarthur/Streamlit-for-Data-Science/blob/main/penguin_app/penguins.csv")
 st.write(penguins_df.head())
