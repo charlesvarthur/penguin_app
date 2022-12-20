97204aad558faaf35b5bc88b545c92ae2409536a
Charles Arthur <charles.v.arthur@gmail.com>
2022-12-20 12:56:29 +0000

    update app syntax

--- penguins.py
+++ penguins.py
@@ -17,6 +17,6 @@ st.write(penguins_df.head())
 st.markdown('Use this Streamlit app to make your own scatterplot about penguins!')
 
 #Create user input variables for the species and characteristics
-selected_species = st.selectbox('What species would you like to visualise?', ['Adelie'], ['Gentoo'], ['Chinstrap'])
-selected_x_var = st.selectbox('What do you want the variable x to be?',['bill_length_mm'],['bill_depth_mm'],['flipper_length_mm'],['body_mass_g'])
-selected_y_var = st.selectbox('What would you like the variable y to be?',['bill_length_mm'],['bill_depth_mm'],['flipper_length_mm'],['body_mass_g'])
+selected_species = st.selectbox('What species would you like to visualise?', ['Adelie', 'Gentoo', 'Chinstrap'])
+selected_x_var = st.selectbox('What do you want the variable x to be?',['bill_length_mm','bill_depth_mm','flipper_length_mm','body_mass_g'])
+selected_y_var = st.selectbox('What would you like the variable y to be?',['bill_length_mm','bill_depth_mm','flipper_length_mm','body_mass_g'])
