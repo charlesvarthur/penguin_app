51fe4fb9fa50d15192c1058fa653552df055bcc5
Charles Arthur <charles.v.arthur@gmail.com>
2022-12-20 16:37:25 +0000

    adjusted ax var parameters

--- penguins.py
+++ penguins.py
@@ -27,7 +27,7 @@ penguins_df = penguins_df[penguins_df['species'] == selected_species]
 sns.set_style=('darkgrid')
 markers = {"Adelie" : 'X', "Gentoo": 's', "Chinstrap":'o'}
 fig, ax = plt.subplots()
-ax = sns.scatterplot(x = penguins_df[selected_x_var], y = penguins_df[selected_y_var],hue ='species',markers = markers)
+ax = sns.scatterplot(data = penguins_df, x = selected_x_var, y = selected_y_var, hue ='species', markers = markers, style ='species')
 plt.title('Characteristics of the {} penguin'.format(selected_species))
 plt.xlabel(selected_x_var)
 plt.ylabel(selected_y_var)
