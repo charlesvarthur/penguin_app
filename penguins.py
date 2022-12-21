df44fb8c16a331d7a914e2c9af7cd19d24dcd3c0
Charles Arthur <charles.v.arthur@gmail.com>
2022-12-21 08:52:55 +0000

    comment out species select box

--- penguins.py
+++ penguins.py
@@ -17,14 +17,14 @@ penguins_df = pd.read_csv('https://raw.githubusercontent.com/charlesvarthur/Stre
 st.markdown('Use this Streamlit app to make your own scatterplot about penguins!')
 
 #Create user input variables for the species and characteristics
-selected_species = st.selectbox('What species would you like to visualise?', ['Adelie', 'Gentoo', 'Chinstrap'])
+#selected_species = st.selectbox('What species would you like to visualise?', ['Adelie', 'Gentoo', 'Chinstrap'])
 selected_x_var = st.selectbox('What do you want the variable x to be?',['bill_length_mm','bill_depth_mm','flipper_length_mm','body_mass_g'])
 selected_y_var = st.selectbox('What would you like the variable y to be?',['bill_length_mm','bill_depth_mm','flipper_length_mm','body_mass_g'])
 
 #Create the scatter plot from the dataframe and the variables
-penguins_df = penguins_df[penguins_df['species'] == selected_species]
+#penguins_df = penguins_df[penguins_df['species'] == selected_species]
 
-sns.set_theme(style = "darkgrid", palette=None)
+sns.set_theme(style = 'darkgrid', palette='deep')
 sns.axes_style("darkgrid")
 markers = {"Adelie" : 'X', "Gentoo": 's', "Chinstrap":'o'}
 fig, ax = plt.subplots()
